fcb136355595ce8b644b7756457fc33bdddd26ad
Lorenz Van Simaeys <lorenz.vansimaeys@student.ucll.be>
2023-04-12 19:10:23 +0200

    finished cells

---
+++ TemperatureConverter/Cells/Cell.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cells
+{
+    public class Cell<T> : INotifyPropertyChanged
+    {
+        private T contents;
+
+        public Cell(T initialContents = default)
+        {
+            this.contents = initialContents;
+        }
+
+        public virtual T Value
+        {
+            get
+            {
+                return contents;
+            }
+            set
+            {
+                this.contents = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Value)));
+            }
+        }
+
+        public Cell<U> Derive<U>(Func<T, U> transformer, Func<U, T> untransformer)
+        {
+            return new Derived<T, U>(this, transformer, untransformer);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class Derived<IN, OUT> : Cell<OUT>
+    {
+        private readonly Cell<IN> dependency;
+        private readonly Func<OUT, IN> untransformer;
+        public Derived(Cell<IN> dependency, Func<IN, OUT> transformer, Func<OUT, IN> untransformer)
+            : base(transformer(dependency.Value))
+        {
+            this.dependency = dependency;
+            this.dependency.PropertyChanged += (sender, args) => base.Value = transformer(dependency.Value);
+            this.untransformer = untransformer;
+        }
+
+        public override OUT Value 
+        { 
+            get => base.Value; 
+            set
+            {
+                dependency.Value = untransformer(value);
+            } 
+        }
+    }
+}
--- TemperatureConverter/ViewModel/ViewModel.cs
+++ TemperatureConverter/ViewModel/ViewModel.cs
@@ -4,37 +4,28 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cells;
 using Model;
+
 namespace ViewModel
 {
-    public class ConverterViewModel : INotifyPropertyChanged
+    public class ConverterViewModel
     {
-        private double temperatureInKelvin;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
         public ConverterViewModel()
         {
+            this.TemperatureInKelvin = new Cell<double>();
+
             this.Kelvin = new TemperatureScaleViewModel(this, new KelvinTemperatureScale());
-            this.Celsius = new TemperatureScaleViewModel(this, new  CelsiusTemperatureScale());
+            this.Celsius = new TemperatureScaleViewModel(this, new CelsiusTemperatureScale());
             this.Fahrenheit = new TemperatureScaleViewModel(this, new FahrenheitTemperatureScale());
         }
 
-        public double TemperatureInKelvin
-        {
-            get
-            {
-                return temperatureInKelvin;
-            }
-            set
-            {
-                temperatureInKelvin = value;
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TemperatureInKelvin)));
-            }
-        }
+        public Cell<double> TemperatureInKelvin { get; }
 
         public TemperatureScaleViewModel Kelvin { get; }
+
         public TemperatureScaleViewModel Celsius { get; }
+
         public TemperatureScaleViewModel Fahrenheit { get; }
 
         public IEnumerable<TemperatureScaleViewModel> Scales
@@ -46,38 +37,27 @@ namespace ViewModel
                 yield return Kelvin;
             }
         }
-
     }
 
-    public class TemperatureScaleViewModel : INotifyPropertyChanged 
+    public class TemperatureScaleViewModel
     {
         private readonly ConverterViewModel parent;
 
         private readonly ITemperatureScale temperatureScale;
 
-        public string Name => temperatureScale.Name;
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-
-        public double Temperature
-        {
-            get
-            {
-                return temperatureScale.ConvertFromKelvin(parent.TemperatureInKelvin);
-            }
-            set
-            {
-                parent.TemperatureInKelvin = temperatureScale.ConvertToKelvin(value);
-            }
-        }
-
-        public TemperatureScaleViewModel(ConverterViewModel parent,  ITemperatureScale temperatureScale)
+        public TemperatureScaleViewModel(ConverterViewModel parent, ITemperatureScale temperatureScale)
         {
             this.parent = parent;
             this.temperatureScale = temperatureScale;
-            this.parent.PropertyChanged += (sender, args) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Temperature)));
+            this.Temperature = this.parent.TemperatureInKelvin.Derive(
+                kelvin => temperatureScale.ConvertFromKelvin(kelvin), 
+                kelvin => temperatureScale.ConvertToKelvin(kelvin)
+                );
         }
+
+        public string Name => temperatureScale.Name;
+        public Cell<double> Temperature { get; }
+
 
     }
 }
